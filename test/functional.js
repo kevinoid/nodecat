bdd10401af602a5528eb65be5a47ffabd03b593e
Kevin Locke <kevin@kevinlocke.name>
2017-02-24 18:09:06 -0700

    Add functional tests
    To be sure the script actually behaves the way it should.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ test/functional.js
@@ -0,0 +1,89 @@
+/**
+ * @copyright Copyright 2017 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+'use strict';
+
+var assert = require('assert');
+var execFile = require('child_process').execFile;
+var fs = require('fs');
+var path = require('path');
+
+var deepEqual = assert.deepStrictEqual || assert.deepEqual;
+var binPath = path.join(__dirname, '..', 'bin', 'nodecat.js');
+var testFiles = [
+  path.join(__dirname, '..', 'doc-src', 'spec', 'footer.xhtml'),
+  path.join(__dirname, '..', 'doc-src', 'spec', 'header.xhtml')
+];
+var testFileContent = testFiles.reduce(function(contents, file) {
+  contents[file] = fs.readFileSync(file);
+  return contents;
+}, {});
+
+describe('nodecat', function() {
+  it('concatenates files around stdin', function(done) {
+    var testContent = new Buffer(256);
+    for (var i = 0; i < 256; i += 1) {
+      testContent[i] = i;
+    }
+
+    var proc = execFile(
+      process.execPath,
+      [
+        binPath,
+        testFiles[0],
+        '-',
+        testFiles[1]
+      ],
+      {encoding: null},
+      function(err, stdout, stderr) {
+        assert.ifError(err);
+        var expected = Buffer.concat([
+          testFileContent[testFiles[0]],
+          testContent,
+          testFileContent[testFiles[1]]
+        ]);
+        deepEqual(stdout, expected);
+        deepEqual(stderr, new Buffer(0));
+        done();
+      }
+    );
+    proc.stdin.end(testContent);
+  });
+
+  it('exits code 1 with error message for non-existent file', function(done) {
+    var testContent = new Buffer(256);
+    for (var i = 0; i < 256; i += 1) {
+      testContent[i] = i;
+    }
+
+    var proc = execFile(
+      process.execPath,
+      [
+        binPath,
+        'nonexistent.txt',
+        testFiles[0],
+        '-',
+        testFiles[1]
+      ],
+      {encoding: null},
+      function(err, stdout, stderr) {
+        assert.strictEqual(err.code, 1);
+
+        // stdout has data that can be read
+        var expected = Buffer.concat([
+          testFileContent[testFiles[0]],
+          testContent,
+          testFileContent[testFiles[1]]
+        ]);
+        deepEqual(stdout, expected);
+
+        // stderr contains an error message
+        assert(/no such file/i.test(String(stderr)));
+        done();
+      }
+    );
+    proc.stdin.end(testContent);
+  });
+});
